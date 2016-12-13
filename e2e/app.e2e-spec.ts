b21f179453976125366052439077e95515634c54
angular-cli <angular-cli@angular.io>
2016-12-13 15:14:05 +0000

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UiRouterAotDemoPage } from './app.po';
+
+describe('ui-router-aot-demo App', function() {
+  let page: UiRouterAotDemoPage;
+
+  beforeEach(() => {
+    page = new UiRouterAotDemoPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
